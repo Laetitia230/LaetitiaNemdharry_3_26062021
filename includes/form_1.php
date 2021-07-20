d05c69fec3e69d87f4b1dbcf0472af42ae908919
Laetitia Nemdharry <laetitia@pc-475.home>
2021-07-20 13:50:01 +0200

    Modif .php

--- includes/form_1.php
+++ includes/form_1.php
@@ -20,5 +20,15 @@
 	$headers .= "Reply-To: $input_504";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
-	return true;			
+	return true;	
+	// Remove query string from static resources
+function remove_cssjs_ver( $src ) 
+{
+if( strpos( $src, '?ver=' ) )
+$src = remove_query_arg( 'ver', $src );
+return $src;
+}
+
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );		
 ?>
